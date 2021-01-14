78598521eff708c0467241d3fb8d6d6dbb586930
IVAN Florin <flory.stay@gmail.com>
2021-01-14 14:00:11 +0100

    disable typescript-eslint/no-explicit-any for index

--- src/shared/utils/breakpoint/index.ts
+++ src/shared/utils/breakpoint/index.ts
@@ -2,5 +2,6 @@ import { BreakPoint } from 'commons/types';
 import Config from 'shared/configuration';
 
 export const getBreakpoint = (key: BreakPoint) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return Config.BREAK_POINTS.filter((k: { [x: string]: any }) => k[key])[0][key];
 };
